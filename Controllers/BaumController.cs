272c6febb6cbfdd99843a6f24d0274bf07deb24a
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2025-09-23 18:00:42 +0200

    123

--- Controllers/BaumController.cs
+++ Controllers/BaumController.cs
@@ -1,10 +1,11 @@
 ﻿using DotNet8.WebApi.Dtos;
 using DotNet8.WebApi.Entities;
+using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Mvc;
 
 namespace DotNet8.WebApi.Controllers
 {
-
+    [Authorize]
     [Route("api/[controller]")]
     [ApiController]
     public class BaumController : ControllerBase    
@@ -13,7 +14,7 @@ namespace DotNet8.WebApi.Controllers
         public async Task<ActionResult<int>> CreateBaum(CreateBaumDto newBaum)
         {
             // Benutzer-ID aus dem JWT-Token im Header extrahieren
-            var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == "sub" || c.Type == "userid" || c.Type == "id");
+            var userIdClaim = User.Claims.FirstOrDefault(c => c.Type == "sub" || c.Type == "userid" || c.Type == "id" || c.Type == "Id");
             if (userIdClaim == null)
             {
                 return Unauthorized("Benutzer-ID nicht im Token gefunden.");
