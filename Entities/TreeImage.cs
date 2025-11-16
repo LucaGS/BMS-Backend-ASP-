eb73e9f85ce4aa4e08a1eb687360796e5f7410d3
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2025-11-16 14:18:12 +0100

    Update Migration File Ü Cleanup

--- Entities/TreeImage.cs
+++
@@ -1,9 +0,0 @@
-namespace DotNet8.WebApi.Entities
-{
-    public class TreeImage
-    {
-        public int Id { get; set; }
-        public string ImageBase64 { get; set; } = string.Empty;
-        public int TreeId { get; set; }
-    }
-}
--- Migrations/20251105191330_InitialCreate.cs
+++ Migrations/20251105191330_InitialCreate.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Net.Mime;
 using Microsoft.EntityFrameworkCore.Migrations;
 using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 
@@ -45,17 +46,20 @@ namespace DotNet8.WebApi.Migrations
                 });
 
             migrationBuilder.CreateTable(
-                name: "TreeImages",
+                name: "Images",
                 columns: table => new
                 {
                     Id = table.Column<int>(type: "integer", nullable: false)
                         .Annotation("Npgsql:ValueGenerationStrategy", NpgsqlValueGenerationStrategy.IdentityByDefaultColumn),
-                    ImageBase64 = table.Column<string>(type: "text", nullable: false),
-                    TreeId = table.Column<int>(type: "integer", nullable: false)
+                    TreeId = table.Column<int>(type: "integer", nullable: false),
+                    FileName = table.Column<string>(type: "text", nullable: false),
+                    ContentType = table.Column<string>(type: "text", nullable: false),
+                    Data = table.Column<byte[]>(type: "bytea", nullable: false)
+
                 },
                 constraints: table =>
                 {
-                    table.PrimaryKey("PK_TreeImages", x => x.Id);
+                    table.PrimaryKey("PK_Images", x => x.Id);
                 });
 
             migrationBuilder.CreateTable(
