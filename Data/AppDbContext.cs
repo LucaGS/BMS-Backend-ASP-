8729579a44c41347e79752640fe13ef91425aa53
Luca Stieme <123728529+LucaGS@users.noreply.github.com>
2025-11-16 14:20:33 +0100

    Cleanup

--- Data/AppDbContext.cs
+++ Data/AppDbContext.cs
@@ -8,7 +8,6 @@ namespace DotNet8.WebApi.Data
         public DbSet<User> Users { get; set; }
         public DbSet<Tree> Trees { get; set; }
         public DbSet<Inspection> Inspections { get; set; }
-        public DbSet<TreeImage> TreeImages { get; set; }
         public DbSet<GreenArea> GreenAreas { get; set; }
         public DbSet<Image> Images { get; set; }
 
